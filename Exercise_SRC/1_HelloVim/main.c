a4c6d40efaf2031558c413d2c2a47d43000fa7c0
SYN <syn@syn-Part.(none)>
2013-01-11 11:22:39 +0800

    First update for test

---
+++ Exercise_SRC/1_HelloVim/main.c
@@ -0,0 +1,24 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  main.c
+ *
+ *    Description:  Hello World of VIM C
+ *
+ *        Version:  1.0
+ *        Created:  01/11/2013 07:44:53 AM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  KnightV (KV), kevinwang1988@smail.hust.edu.cn
+ *   Organization:  FSF
+ *
+ * =====================================================================================
+ */
+#include<stdio.h>
+int main()
+{
+  printf("Hello, my first VIM C");
+  return 0;
+}
+
---
+++ Exercise_SRC/2_CalcPower/cal.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main()
+
+{
+  int a, b, c, d, count = 0;
+  for(a = 0; a <= 3; a++)
+    for(b = 0; b <= 9; b++)
+      for(c = 0; c <= 9; c++)
+        for(d = 0; d <= 9; d++)
+          if((a + b + c + d)%4 == 0)
+             count ++;
+          else
+             continue;
+  printf("%d\n", count );
+  return 0;
+
+
+
+
+}

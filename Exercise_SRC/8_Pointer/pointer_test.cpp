d00b1ed383ace2fe94df5f13ea9adb9925fbd0e5
syn <syn@syn.(none)>
2014-04-04 05:25:22 +0800

    Add pointer test source

---
+++ Exercise_SRC/8_Pointer/pointer_test.cpp
@@ -0,0 +1,30 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  pointer_test.cpp
+ *
+ *    Description:  pointer test source
+ *
+ *        Version:  1.0
+ *        Created:  04/04/2014 04:39:09 AM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  MZ (0xFFFF), 
+ *   Organization:  Wasteland
+ *
+ * =====================================================================================
+ */
+
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+int main()
+{
+  int* p;
+  int* pa = 0;
+  int v = 1;
+  pa = &v;
+
